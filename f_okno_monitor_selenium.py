d8bf769d8189ddda9a93315eb1e2a1aec2680885
refactor-shade <kate.erygina@gmail.com>
2025-10-31 18:41:45 +0100

    Update f_okno_monitor_selenium.py

--- f_okno_monitor_selenium.py
+++ f_okno_monitor_selenium.py
@@ -12,10 +12,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 from telegram import Bot
 
